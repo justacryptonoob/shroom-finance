b72cc1def0402e330b285de76eb0a30f162d4e27
justacryptonoob <73740065+justacryptonoob@users.noreply.github.com>
2020-12-10 20:47:37 +0700

    Add files via upload

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,1 +1,95 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  SHROOM,
+  Approval,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/SHROOM/SHROOM"
+import { Approve, OwnershipTransfered, Transfered } from "../generated/schema"
 
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Approve.load(event.params.value.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new Approve(event.params.value.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.BURNER_ROLE(...)
+  // - contract.DEFAULT_ADMIN_ROLE(...)
+  // - contract.MINTER_ROLE(...)
+  // - contract.PAUSER_ROLE(...)
+  // - contract.allowance(...)
+  // - contract.approve(...)
+  // - contract.balanceOf(...)
+  // - contract.decimals(...)
+  // - contract.decreaseAllowance(...)
+  // - contract.getRoleAdmin(...)
+  // - contract.getRoleMember(...)
+  // - contract.getRoleMemberCount(...)
+  // - contract.hasRole(...)
+  // - contract.increaseAllowance(...)
+  // - contract.name(...)
+  // - contract.paused(...)
+  // - contract.symbol(...)
+  // - contract.totalSupply(...)
+  // - contract.transfer(...)
+  // - contract.transferFrom(...)
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let entity = OwnershipTransfered.load(event.params.newOwner.toHex())
+
+  if (entity == null) {
+    entity = new OwnershipTransfered(event.params.newOwner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let transfer = new Transfered(event.transaction.hash.toHex())
+  transfer.blockNumber = event.block.number
+  transfer.value =  event.params.value
+  transfer.from = event.params.from
+  transfer.to = event.params.to
+  transfer.value = event.transaction.value
+  transfer.timestamp = event.block.timestamp
+  transfer.transactionHash =event.transaction.hash
+  transfer.save()
+}
